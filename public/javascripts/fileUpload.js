9d5cf85f3aeca6e8a6347e9b2495f570183ea25d
phiteh191 <phiteh191@studentmail.oru.se>
2023-07-16 16:29:31 +0200

    Finished all css for the webpage

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,14 +1,37 @@
+/* This gets all css style roots that have the :root*/
+const rootStyles = window.getComputedStyle(document.documentElement);
+
+/* Make sure that it's not undefined*/
+if (
+  rootStyles.getPropertyValue("--book-cover-width-large") != null &&
+  rootStyles.getPropertyValue("--book-cover-width-large") !== ""
+) {
+  ready();
+} else {
+  /* If it is not defined then wait untill the css is loaded and run ready function*/
+  document.getElementById("main-css").addEventListener("load", ready);
+}
+
 // Settings for FilePond, uploading book covers
-FilePond.registerPlugin(
-  FilePondPluginImagePreview,
-  FilePondPluginImageResize,
-  FilePondPluginFileEncode
-);
+function ready() {
+  const coverWidth = parseFloat(
+    rootStyles.getPropertyValue("--book-cover-width-large")
+  );
+  const coverAspectRatio = parseFloat(
+    rootStyles.getPropertyValue("--book-cover-aspect-ratio")
+  );
+  const coverHeigt = coverWidth / coverAspectRatio;
+  FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginFileEncode
+  );
 
-FilePond.setOptions({
-  stylePanelAspectRatio: 150 / 100,
-  imageResizeTargetWidth: 100,
-  imageResizeTargetHeight: 150,
-});
+  FilePond.setOptions({
+    stylePanelAspectRatio: 1 / coverAspectRatio,
+    imageResizeTargetWidth: coverWidth,
+    imageResizeTargetHeight: coverHeigt,
+  });
 
-FilePond.parse(document.body);
+  FilePond.parse(document.body);
+}
